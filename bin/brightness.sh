589ab67bb1de930639acaaf485d7082000a20dcd
Volodymyr Volkov <volodymyr.volkov@nsn.com>
2015-01-13 20:27:25 +0100

    brightness corrected with proper max_brightness value

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+MAX_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 BRIGHTNESS_PERCENT=$1
-sudo tee /sys/class/backlight/intel_backlight/brightness <<< $(echo "${BRIGHTNESS_PERCENT}*3484/100" | bc) > /dev/null
+sudo tee /sys/class/backlight/intel_backlight/brightness <<< $(echo "${BRIGHTNESS_PERCENT}*${MAX_BRIGHTNESS}/100" | bc) > /dev/null
